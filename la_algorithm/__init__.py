c53319c02b2686d89be5720c8775fea3d4a34d07
Paul Todd <paulxtodd@gmail.com>
2019-05-10 13:15:23 +0100

    Refactor

---
+++ la_algorithm/__init__.py
@@ -0,0 +1,21 @@
+"""
+Collection of fibonacci methods and functions
+"""
+from . import fibonaci
+
+__version__ = '1.0.3'
+__author__ = 'Omkar Pathak'
+
+# List maintainers here
+__maintainers__ = [
+    'Lucy Todd',
+    'Paul Todd'
+]
+# List contributors here
+__contributors__ = [
+	"Lucy 'LucyTodd' Todd",
+	"Paul 'PaulTodd' Todd"
+]
+__all__ = [
+    'fibonaci'
+]
